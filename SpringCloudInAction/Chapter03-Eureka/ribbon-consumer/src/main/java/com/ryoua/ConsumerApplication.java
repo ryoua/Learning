cdb0ac9077259b053838b553be564947a01ca19f
ryoua <1794970183@qq.com>
2020-04-19 21:49:14 +0800

    完成服务消费和负载均衡

---
+++ SpringCloudInAction/Chapter03-Eureka/ribbon-consumer/src/main/java/com/ryoua/ConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.ryoua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: RyouA
+ * @Date: 2020/4/19 - 8:58 下午
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
