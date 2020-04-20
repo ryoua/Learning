6d9b55aed73520a06254fb834a9ee776a33c94e4
ryoua <1794970183@qq.com>
2020-04-20 15:31:18 +0800

    添加《现代操作系统》

---
+++ OS/library.c
@@ -0,0 +1,14 @@
+#include "library.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
+
+int main() {
+    hello();
+    return 0;
+}
+
+
---
+++ OS/library.h
@@ -0,0 +1,6 @@
+#ifndef OS_LIBRARY_H
+#define OS_LIBRARY_H
+
+void hello(void);
+
+#endif //OS_LIBRARY_H
