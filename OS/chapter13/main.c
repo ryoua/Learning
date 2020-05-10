3bf48b8a7eafa286c6dc0294b8726e8a962022da
ryoua <1794970183@qq.com>
2020-05-10 09:52:13 +0800

    finished chapter13

---
+++ OS/chapter13/main.c
@@ -0,0 +1,10 @@
+    #include "main.h"
+
+    int main() {
+        printf("location of code: %p\n", (void *) main);
+        printf("location of heap: %p\n", (void *) malloc(1));
+
+        int x = 3;
+        printf("location of stack: %p\n", (void *) &x);
+        return x;
+    }
