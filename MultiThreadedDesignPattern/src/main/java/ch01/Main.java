b8f81cc1c1a27584ccfde94898a459994646f197
ryoua <1794970183@qq.com>
2020-05-14 16:01:27 +0800

    添加《多线程设计模式》

---
+++ MultiThreadedDesignPattern/src/main/java/ch01/Main.java
@@ -0,0 +1,12 @@
+package ch01;
+
+/**
+ * @Author: RyouA
+ * @Date: 2020/5/14 - 3:58 下午
+ **/
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++)
+            System.out.println(i);
+    }
+}
